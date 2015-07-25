d7c85e7d86f3c40fb194a059ed8a0af233ea9793
Kevin Lustig <kevin@thejourneylab.com>
2015-07-25 21:21:43 +0400

    Adding logic to toggle between "Web Map" URL and other doc types' file upload/edit, close #16, close #17

--- client/js/assets.js
+++ client/js/assets.js
@@ -29,6 +29,36 @@ var assetCtrl = {
 			}
 			return options.inverse(this);
 		})
+		Handlebars.registerHelper('neq',function(v1,v2,options) {
+			if (v1 && v2 && v1.toString() !== v2.toString()) {  
+				return options.fn(this);
+			}
+			return options.inverse(this);
+		})
+	},
+	view: {
+		editAsset: {
+			type: undefined,
+			file: undefined,
+			url: undefined
+		}
+	},
+	model: {
+		editAsset: undefined
+	},
+	handleType: function() {
+		var that = this;
+		var view = this.view.editAsset;
+		view.type.on("change",function() {
+			var val = $(this).val();
+			if (val == "webmap") {
+				view.file.removeClass("active");
+				view.url.addClass("active");
+			} else {
+				view.url.removeClass("active");
+				view.file.addClass("active");
+			}
+		})
 	}
 }
 
@@ -41,21 +71,23 @@ $(function() {
 			$("#edit-asset").attr("action","/asset/"+id);
 			$.getJSON("/api/asset/"+id,function(result) {
 				result.response.opts = localConfig.asset_opts;
-				$("#edit-asset .modal-body").html(assetCtrl.templates.editAsset.tpl(result.response));
+				assetCtrl.model.editAsset = result.response;
+				$("#edit-asset .modal-body").html(assetCtrl.templates.editAsset.tpl(assetCtrl.model.editAsset));
 				$("#edit-asset").validate();
+				assetCtrl.view.editAsset.type = $("#edit-asset [name=type]");
+				assetCtrl.view.editAsset.file = $("#edit-asset #form-file");
+				assetCtrl.view.editAsset.url = $("#edit-asset #form-url");
+				assetCtrl.handleType();
 			})
 		})		
 	})
 	assetCtrl.prepTemplates();
-	/*$("#add-toggle").click(function() {
-		$(this).toggleClass("btn-success");
-		$("#add-asset").toggleClass("open");
-	})*/
 	$(".delete-toggle").click(function() {
 		$("#delete-asset").attr("action","/asset/"+$(this).attr("rel"));
 	})
 	
 	$("table").DataTable({
+		"order": [[0,"desc"]],
       "aoColumnDefs": [
           { 'bSortable': false, 'aTargets': [ -1 ] }
        ]
