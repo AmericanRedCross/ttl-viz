c9657375045a5f90b24be313667f387a9ccd66df
Dan Joseph <danbjoseph@gmail.com>
2017-02-13 11:10:13 -0500

    user-auth starting point

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
-### ttl-viz
+# user-auth
+express, handlebars, sqlite, passport, foundation
 
-- `sudo npm install` to get all the node modules
-- change the permissions of the backups and tmp folders so the app can write to it `sudo chown -R ubuntu:ubuntu /ttl-viz/backups/` and `sudo chown -R ubuntu:ubuntu /ttl-viz/tmp/`
-- create a config.js file from config.js.example and adjust settings for your deployment
-- start mongo and postgres
-- install imagemagick and graphicsmagick on your system
-  - `sudo apt-get install imagemagick`
-  - `sudo apt-get install graphicsmagick`
-- for image uploads with nginx you'll need to adjust your configuration settings http://craftcms.stackexchange.com/a/2330
+
+### permission levels
+- user
+- editor
+- admin
+  - all lower permissions, add/edit users
---
+++ app.js
@@ -0,0 +1,237 @@
+var settings = require('./settings.js');
+var fs = require('fs');
+var path = require('path');
+var crypto = require('crypto');
+var sqlite3 = require('sqlite3');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+// initialize database
+var file = path.join(settings.app.db);
+fs.existsSync(file);
+var db = new sqlite3.Database(file);
+
+// initialize db table for admin users
+db.run("CREATE TABLE IF NOT EXISTS users ( id INTEGER PRIMARY KEY AUTOINCREMENT, user TEXT, password TEXT, permissions TEXT )", function(err) {
+    if(err) { console.log(err); }
+    db.get("SELECT user from users", function(err, row) {
+      if(err) { console.log(err); }
+      if(!row) {
+        bcrypt.hash(settings.app.defaultpass, saltRounds, function(err, hash) {
+          db.run("INSERT INTO users ( user, password, permissions ) VALUES( ?, ?, 'admin' )", settings.app.defaultuser, hash, function(err) {
+            if(err) { console.log(err); }
+          });
+        });
+      }
+    });
+});
+
+// db functions
+var createUser = function(req, res) {
+  var user = req.body.user;
+  var permissions = req.body.permissions;
+  db.get('SELECT user FROM users WHERE user = ?', user, function(err, row) {
+    // if(err)
+    if(row) {
+      req.flash('errorMessage', " there is already a user with that name");
+      res.redirect('/admin/users');
+    } else {
+      bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        db.run('INSERT INTO users ( user, password, permissions ) VALUES( ?, ?, ? )', user, hash, permissions, function(err) {
+          // if(err)
+          if(this.lastID) {
+            req.flash('successMessage', " user created");
+            res.redirect('/admin/users');
+          } else {
+            req.flash('errorMessage', " something went wrong");
+            res.redirect('/admin/users');
+          }
+        });
+      });
+    }
+  });
+}
+
+var deleteUser = function(req, res) {
+  if(req.user.id == req.body.id) {
+    req.flash('errorMessage', " you can't delete yourself");
+    res.redirect('/admin/users');
+  } else {
+    db.run('DELETE FROM users WHERE id = ?', req.body.id, function(err) {
+      if(err) {
+        //...
+      } else {
+        req.flash('successMessage', " user deleted");
+        res.redirect('/admin/users');
+      }
+    });
+  }
+}
+
+
+var editUser = function(req, res) {
+  var runquery = false;
+  var query = "UPDATE users SET ";
+  if(req.body.password.length == 0) {
+    req.flash('successMessage', " password not changed");
+  } else {
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+      // if(err)
+      query += "password = " + hash + ", " ;
+      runquery = true;
+    });
+  }
+  if(req.user.id == req.body.id && req.body.permissions != "admin") {
+    req.flash('errorMessage', " you can't remove your own admin status");
+  } else {
+    query += "permissions = '" + req.body.permissions + "'";
+    runquery = true;
+  }
+  query += " WHERE id = " + req.body.id;
+  if(runquery == true) {
+    db.run(query, function(err) {
+      if(err) {
+        //...
+        console.log(err);
+        req.flash('errorMessage', " something went wrong");
+        res.redirect('/admin/users');
+      } else {
+        req.flash('successMessage', " user updated");
+        res.redirect('/admin/users');
+      }
+    });
+  } else {
+    req.flash('errorMessage', " something went wrong");
+    res.redirect('/admin/users');
+  }
+
+}
+
+var listUsers = function(cb) {
+  db.all('SELECT id, user, permissions FROM users', function(err, rows) {
+    // if(err)
+    cb(rows);
+  });
+}
+
+// setting up user authentication
+passport.use(new LocalStrategy({ usernameField: 'user' }, function(user, password, done) {
+  db.get('SELECT password FROM users WHERE user = ?', user, function(err, row) {
+    if (!row) return done(null, false);
+    bcrypt.compare(password, row.password, function(err, res) {
+      if(!res) return done(null, false);
+      db.get('SELECT user, id FROM users WHERE user = ?', user, function(err, row) {
+        return done(null, row);
+      });
+    });
+  });
+}));
+
+passport.serializeUser(function(user, done) {
+  return done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  db.get('SELECT id, user, permissions FROM users WHERE id = ?', id, function(err, row) {
+    if (!row) { return done(null, false); }
+    return done(null, row);
+  });
+});
+
+// setting up the app
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var SQLiteStore = require('connect-sqlite3')(session);
+var flash = require('express-flash');
+var exphbs  = require('express-handlebars');
+var bodyParser = require('body-parser');
+
+var app = express();
+// bodyParser to let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(cookieParser('secret'));
+app.use(session({
+  store: new SQLiteStore,
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 60 * 60 * 1000 } // 1 hour
+}));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main',
+  helpers: {
+    eq: function(v1,v2,options) {
+			if (v1 && v2 && v1.toString() === v2.toString()) {
+				return options.fn(this);
+			}
+			return options.inverse(this);
+		}
+  }
+}));
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+app.post('/login', passport.authenticate('local', {
+    failureRedirect: '/'
+  }), function(req, res) {
+    res.redirect('/');
+  }
+);
+
+app.post('/logout', function(req, res) {
+	req.session.destroy(function(err) {
+		res.redirect('/');
+	})
+});
+
+app.get('/', function(req, res) {
+		res.render('home',{
+			user:req.user
+		});
+});
+
+app.get('/admin/users', function(req, res) {
+  if(req.user && req.user.permissions == "admin") {
+    listUsers(function(result) {
+      res.render('users',{
+        user:req.user,
+        users: result,
+        error:req.flash("errorMessage"),
+        success:req.flash("successMessage")
+      });
+    });
+  } else {
+    res.redirect('/');
+  }
+});
+
+app.post('/admin/user', function(req, res) {
+  if (req.user && req.user.permissions == "admin") {
+    switch(req.body["_method"]) {
+      case "DELETE":
+        deleteUser(req, res);
+      break;
+      case "PUT":
+        editUser(req, res);
+      break;
+      default:
+        createUser(req, res);
+      break;
+    }
+  } else { res.redirect('/admin/users'); }
+});
+
+app.listen(settings.app.port, function() {
+  console.log('app listening on port ' + settings.app.port);
+});
